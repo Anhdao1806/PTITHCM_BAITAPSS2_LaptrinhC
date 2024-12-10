4c9e8cfc3fce4c3e0714df1c22255d1b7b9d9ea4
Anhdao1806 <daoleho123@gmail.com>
2024-12-10 19:22:06 +0700

    Add files via upload

---
+++ baitap5ss2_laptrinhC/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int chieudai = 9;
+    int chieurong= 4;
+    int chu_vi = (chieudai + chieurong)* 2;
+    int dien_tich = chieudai * chieurong ;
+    printf ("chu_vi= %d\n", chu_vi );
+    printf ( "dien_tich = %d", dien_tich);
+    return 0;
+}
+    
+
