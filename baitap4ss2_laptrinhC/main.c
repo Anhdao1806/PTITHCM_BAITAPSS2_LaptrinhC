65ca59e9e738a81794e7dcaeab2615fb904ced1f
Anhdao1806 <daoleho123@gmail.com>
2024-12-10 19:12:15 +0700

    Add files via upload

---
+++ baitap4ss2_laptrinhC/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    int canh = 5;
+    //dien tich hinh vuong
+    int dien_tich= canh * 4 ;
+    //chu vi hinh vuong
+    int chu_vi= canh * canh ;
+    
+    printf ("dien_tich = %d\n", dien_tich);
+    printf ("chu_vi= %d\n", chu_vi );
+    return 0;
+}
+
