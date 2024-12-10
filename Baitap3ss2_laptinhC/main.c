e2c5a0da7daa7668a35be3db7c4eb56d2be9619e
Anhdao1806 <daoleho123@gmail.com>
2024-12-10 19:00:59 +0700

    Add files via upload

---
+++ Baitap3ss2_laptinhC/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 4, b = 2;
+    int tong = a + b;
+    int hieu = a - b;
+    int tich = a * b;
+    float thuong  = a / b;
+    printf("tong = %d\n", tong);
+    printf("hieu = %d\n", hieu);
+    printf("tich = %d\n", tich);
+    printf("thuong = %.2f",thuong);
+    return 0;
+}
+
