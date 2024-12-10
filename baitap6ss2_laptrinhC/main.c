534c0ecbe218896a025b61b34289f448f9f7ed5a
Anhdao1806 <daoleho123@gmail.com>
2024-12-10 19:28:06 +0700

    Add files via upload

---
+++ baitap6ss2_laptrinhC/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float pi=3.14;
+    float ban_kinh=12;
+    float chu_vi= 2 * pi * ban_kinh;
+    float dien_tich= pi* ban_kinh * ban_kinh;
+    printf("chu_vi = %.2f\n", chu_vi);
+    printf("dien_tich = %.2f\n", dien_tich);
+    return 0;
+}
+
